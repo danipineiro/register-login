ef681929f839cd2bc708a024de1aca2ac925e4c8
Daniel Piñeiro Placer <38256338+danipineiro@users.noreply.github.com>
2024-03-26 18:16:24 +0100

    signin and token refresh

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from .views import UserSignUpView
 
 urlpatterns = [
     path('signup/', UserSignUpView.as_view(), name='signup'),
+    path('signin/', TokenObtainPairView.as_view(), name='signin'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
